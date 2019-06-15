cc74b977d509107231a3dac3ca639b43172895bf
mito_log <hkyhm5l@gmail.com>
2019-06-15 15:00:09 +0700

    Initial Commit

---
+++ FitCloak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitCloak
+//
+//  Created by Yuhei Miyazato on 2019/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
